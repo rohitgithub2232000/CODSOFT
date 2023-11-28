2b44d6c246728356550dc61fe4bcbb2991e5cbc4
rohit kalkhaire <rohitkalkhaire22@gmail.com>
2023-11-28 10:52:52 +0530

    project added

---
+++ Task 3a E-commerce website/server/url.js
@@ -0,0 +1,3 @@
+//  module.exports = `mongodb://localhost:27017`
+//module.exports = `mongodb+srv://admin:admin@mdb.vtkja.mongodb.net`
+module.exports = `mongodb+srv://admin:admin@cluster0.ajgt0kp.mongodb.net`
